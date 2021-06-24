4b61ed9a091cbe1fabbc3e287d57bd27b9891262
Burhan Jawed <BurhanJawed@gmail.com>
2021-06-24 06:39:48 -0400

    changed localURI to remoteURI pointing to db on mongoDB Atlas

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
-exports.LocalURI = "mongodb://localhost/books229";
-exports.RemoteURI = "your remote host information goes here";
+exports.Secret = exports.HostName = exports.RemoteURI = void 0;
+exports.RemoteURI = "mongodb+srv://burhan:H78pMISWSTDqeu2z@mongodbserver.0bnbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.HostName = "LocalHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,6 @@
-export const LocalURI = "mongodb://localhost/books229";
-export const RemoteURI = "your remote host information goes here"
+//export const LocalURI = "mongodb://localhost/books229";
+
+// connect to remote URI from MongoDB Atlas
+export const RemoteURI = "mongodb+srv://burhan:H78pMISWSTDqeu2z@mongodbserver.0bnbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 export const HostName = "LocalHost";
 export const Secret = "someSecret";

0ebca10a1a36708e716826c45cce37f160d3bfb7
Burhan Jawed <BurhanJawed@gmail.com>
2021-06-24 08:15:35 -0400

    edited db config hostname to RemoteHost

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = void 0;
 exports.RemoteURI = "mongodb+srv://burhan:H78pMISWSTDqeu2z@mongodbserver.0bnbr.mongodb.net/book_list?retryWrites=true&w=majority";
-exports.HostName = "LocalHost";
+exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -2,5 +2,5 @@
 
 // connect to remote URI from MongoDB Atlas
 export const RemoteURI = "mongodb+srv://burhan:H78pMISWSTDqeu2z@mongodbserver.0bnbr.mongodb.net/book_list?retryWrites=true&w=majority"
-export const HostName = "LocalHost";
+export const HostName = "RemoteHost";
 export const Secret = "someSecret";
